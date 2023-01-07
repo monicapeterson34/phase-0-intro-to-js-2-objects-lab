13b2a565a4ae8f0be8a8040f898b05f1c6db98ef
Monica Peterson <monicapeterson2749@gmail.com>
2023-01-07 17:11:23 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+// Write your solution in this for
+
+const employee = {
+    name : "Sam",
+    streetAddress : "1200 East Berkeley Drive",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee1 = {...employee};
+    delete newEmployee1[key];
+    return newEmployee1;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const newEmployee2 = employee;
+    delete newEmployee2[key];
+    return newEmployee2;
+}
